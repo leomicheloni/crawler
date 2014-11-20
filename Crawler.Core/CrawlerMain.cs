6013fd2a276741e4e6e9df4d5064081de78e901e
Leonardo <leomicheloni@hotmail.com>
2014-11-19 22:39:55 -0300

    new WebPage abstraction

--- Crawler.Core/CrawlerMain.cs
+++ Crawler.Core/CrawlerMain.cs
@@ -8,36 +8,17 @@ namespace Crawler.Core
 
         public CrawlerMain()
         {
-
+            
         }
 
         public void GetRootPage()
         {
-            var url = "http://ae.doctoruna.com/en/doctors/a";
-            HttpWebRequest oReq = (HttpWebRequest)WebRequest.Create(url);
-            oReq.UserAgent = @"Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5";
-
-            HttpWebResponse resp = (HttpWebResponse)oReq.GetResponse();
-
-            var doc = new HtmlAgilityPack.HtmlDocument();
+            foreach(var page in WebPage.GetAllPagesUnder(new Uri("http://ae.doctoruna.com/en/doctors/a"))){
+                Console.WriteLine( page.Url);
+            }
             
-
-            try
-            {
-                var resultStream = resp.GetResponseStream();
-                doc.Load(resultStream); // The HtmlAgilityPack
-                //result = new Internal() { Url = url, HtmlDocument = doc };
-            }
-            catch (System.Net.WebException ex)
-            {
-               // result = new WebPage.Error() { Url = url, Exception = ex };
-            }
-            catch (Exception ex)
-            {
-                ex.Data.Add("Url", url);    // Annotate the exception with the Url
-                throw;
-            }
-
+            
+            
         }
 
     }
