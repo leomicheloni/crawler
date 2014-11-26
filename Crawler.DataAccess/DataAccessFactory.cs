3c4b48626c2594ab1573fab2b89260f8309a6407
Leonardo <leomicheloni@hotmail.com>
2014-11-25 22:22:07 -0300

    added data access project

---
+++ Crawler.DataAccess/DataAccessFactory.cs
@@ -0,0 +1,17 @@
+﻿using Crawler.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler.DataAccess
+{
+    public class DataAccessFactory
+    {
+        public IRepository<DoctorInfo> Build()
+        {
+            return new FileSystemRepository();
+        }
+    }
+}
---
+++ Crawler.DataAccess/FileSystemRepository.cs
@@ -0,0 +1,26 @@
+﻿using Crawler.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler.DataAccess
+{
+    public class FileSystemRepository: IRepository<DoctorInfo>
+    {
+        public void SaveAll(IEnumerable<DoctorInfo> list)
+        {
+            var path = @"F:\temp\doctos_data.txt";
+            var builder = new StringBuilder();
+            foreach (var item in list)
+            {
+                builder.Append(item.ToString());
+                builder.Append("=====================");
+            }
+
+            File.WriteAllText(path, builder.ToString());
+        }
+    }
+}
